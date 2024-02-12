075b73f2bf90d0a1b12237b294fb04a928b7a66a
Farshid Keivanian <87278222+FarshidKeivanian@users.noreply.github.com>
2024-02-12 12:52:22 +1100

    Add files via upload

---
+++ Algorithm for Bubble Sort algorithm_Python.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n-1):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+
+# Example usage
+numbers = [64, 34, 25, 12, 22, 11, 90]
+sorted_numbers = bubble_sort(numbers)
+print("Sorted list:", sorted_numbers)
+
+
+# In[ ]:
+
+
+
+
