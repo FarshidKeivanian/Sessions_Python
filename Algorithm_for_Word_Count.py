a834a3a9afb2f048f9bfc17126e930710b9d74dd
Farshid Keivanian <87278222+FarshidKeivanian@users.noreply.github.com>
2024-03-11 22:18:56 +1100

    Update Algorithm_for_Word_Count.py

--- Algorithm_for_Word_Count.py
+++ Algorithm_for_Word_Count.py
@@ -5,6 +5,9 @@ conf = SparkConf().setAppName("WordCount").setMaster("local")
 sc = SparkContext.getOrCreate(conf=conf)
 
 # Your existing code with SparkContext `sc` now defined
+# textFile = sc.textFile("hdfs://path/to/text/file")
+# This line tells Spark to create an RDD by reading a text file from HDFS (Hadoop Distributed File System).
+# The path "hdfs://path/to/text/file" is where your text file is located on the HDFS.
 textFile = sc.textFile('C:\\example.txt')
 wordCounts = textFile.flatMap(lambda line: line.split(" ")) \
              .map(lambda word: (word, 1)) \
