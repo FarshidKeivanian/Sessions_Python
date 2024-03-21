0d8bf68472d76962531c540e96d6d18e5d9d3cf7
Farshid Keivanian <87278222+FarshidKeivanian@users.noreply.github.com>
2024-03-21 20:51:27 +1100

    Add files via upload
    Hi,
    
    This is Sydney Temperature Standard Deviation.
    
    Best regards,
    Farshid

---
+++ Sydney_Temperature_Standard Deviation.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+# Let's say we have the average temperatures in degrees Celsius for a week in Sydney
+sydney_temperatures = [22.0, 23.5, 21.5, 24.0, 22.5, 23.0, 21.0]
+
+# Calculate the standard deviation
+standard_deviation = np.std(sydney_temperatures)
+
+standard_deviation
