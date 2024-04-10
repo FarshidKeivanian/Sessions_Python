49f2e0aca269892b34fb33011633c8995750f65e
Dr. Farshid Keivanian <87278222+FarshidKeivanian@users.noreply.github.com>
2024-04-10 21:45:54 +1000

    Update Algorithm_for_Torrens Cafe Ordering System.py

--- Algorithm_for_Torrens Cafe Ordering System.py
+++ Algorithm_for_Torrens Cafe Ordering System.py
@@ -1,6 +1,5 @@
 # Importing necessary libraries
 import sys
-
 # Display welcome message and menu
 print("-" * 30)
 print("\tWelcome to Torrens Cafe")
@@ -14,9 +13,9 @@ print("6. Coffee ($1.09 per cup)")
 print("Enter 111 to finalize the payment.")
 print("Enter 000 to quit the program.")
 print("\n")
-
-# Initialize the total cost variable
+# Initialize the total cost variable and items list
 total = 0.0
+items = []
 
 # Loop until user decides to quit or finalize the payment
 while True:
@@ -25,35 +24,34 @@ while True:
         
         # Handling the exit code
         if item_choice == '000':
-            print("Quitting")
-            sys.exit(0)  # Exit the program
+            print("Quitting the program...")
+            break  # Break out of the while loop to quit the program
 
         # Finalize payment and break the loop
         elif item_choice == '111':
+            print("Here's what you ordered:")
+            for item in items:
+                print(f"{item['quantity']} x {item['name']} for ${item['price'] * item['quantity']:.2f}")
             print(f"Total: ${total:.2f}")
             break
 
         # Process item choices
         elif item_choice in ['1', '2', '3', '4', '5', '6']:
             quant = int(input("Quantity: "))
-
-            # Calculate cost based on the choice
-            if item_choice == '1':
-                total += 10 * quant
-            elif item_choice == '2':
-                total += 15 * quant
-            elif item_choice == '3':
-                total += 20 * quant
-            elif item_choice == '4':
-                total += 0.99 * quant
-            elif item_choice == '5':
-                total += 0.79 * quant
-            elif item_choice == '6':
-                total += 1.09 * quant
+            menu = {
+                '1': ('Small brekky', 10),
+                '2': ('Regular brekky', 15),
+                '3': ('Big brekky', 20),
+                '4': ('Egg', 0.99),
+                '5': ('Toast', 0.79),
+                '6': ('Coffee', 1.09)
+            }
+            name, price = menu[item_choice]
+            total += price * quant
+            items.append({'name': name, 'quantity': quant, 'price': price})
         else:
             print("Enter a valid input again!")
 
     # Handle exceptions for non-integer quantities and invalid choices
     except ValueError:
         print("Enter a valid input again!")
-
