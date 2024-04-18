ae28bf8d5381c9116f05eb6bafa88fcbab73b7f9
Dr. Farshid Keivanian <87278222+FarshidKeivanian@users.noreply.github.com>
2024-04-19 07:01:38 +1000

    Add files via upload
    Greeting, Students!
    Happy a Learning Day!
    Farshid

---
+++ Algorithm_for_web_scraper.py
@@ -0,0 +1,23 @@
+
+import requests
+from bs4 import BeautifulSoup
+
+# The URL of the website we want to scrape data from
+url = 'https://www.abc.net.au/news'
+
+# Sending an HTTP GET request to the website
+response = requests.get(url)
+
+# Parsing the HTML using BeautifulSoup
+soup = BeautifulSoup(response.text, 'html.parser')
+
+# Finding all the news headlines on the page (assuming each headline is contained in an <h2> tag)
+headlines = soup.find_all('h2')
+
+# Opening a file to write
+with open('headlines.txt', 'w', encoding='utf-8') as file:
+    for headline in headlines:
+        # Writing each headline into the file
+        file.write(headline.text.strip() + '\n')
+
+print('Headlines have been saved in the file headlines.txt.')
