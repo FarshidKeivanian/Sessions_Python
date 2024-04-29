ff1d298a1ee8cecb561832a78626845f625214f7
Dr. Farshid Keivanian <87278222+FarshidKeivanian@users.noreply.github.com>
2024-04-29 21:42:09 +1000

    Add files via upload
    Greeting, Students!
    This code represents: a three-dimensional view showing the reach of a robotic arm's end effector to a specific target position.
    Happy a Learning Day!
    https://keivan-rc.com/
    https://www.linkedin.com/in/farshid-keivanian/

---
+++ 3D Visualization of Robotic Arm Reach.py
@@ -0,0 +1,17 @@
+from mpl_toolkits.mplot3d import Axes3D
+
+# Define the target position vector for the robotic arm's end effector
+target_vector = np.array([3, 2, 5])
+
+# 3D plot for the robotic arm's reach
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.quiver(0, 0, 0, *target_vector, length=np.linalg.norm(target_vector), color='red')
+ax.set_xlim([0, 6])
+ax.set_ylim([0, 6])
+ax.set_zlim([0, 6])
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+plt.title('Robotic Arm End Effector Position')
+plt.show()
