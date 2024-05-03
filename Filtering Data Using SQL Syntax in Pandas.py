ed86e69efa831a73affef4b3b70cdeec3065d754
Dr. Farshid Keivanian <87278222+FarshidKeivanian@users.noreply.github.com>
2024-05-04 00:37:47 +1000

    Add files via upload

---
+++ Filtering Data Using SQL Syntax in Pandas.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from pandasql import sqldf
+
+pysql = lambda q: sqldf(q, globals())
+
+df = pd.DataFrame({
+    "Name": ["John Doe", "Jane Doe"],
+    "Age": [30, 25]
+})
+
+
+query = """
+SELECT *
+FROM df
+WHERE Age > 25
+"""
+
+result_df = pysql(query)
+
+print(result_df)
