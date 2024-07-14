c7f2341b6f759befe94cc4512f2545983829a174
Dr. Farshid Keivanian <87278222+FarshidKeivanian@users.noreply.github.com>
2024-07-15 08:07:22 +1000

    Add files via upload

---
+++ Lecture 1_Basic Python Script_Greeting and Arithmetic.py
@@ -0,0 +1,12 @@
+# This is a simple Python script
+
+# Print a greeting
+print("Hello, World!")
+
+# Perform a basic arithmetic operation
+a = 5
+b = 3
+sum = a + b
+
+# Print the result
+print("The sum of", a, "and", b, "is", sum)
