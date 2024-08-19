6a15a4c55013a22b62d66b11f46aa97eebe0d662
Dr. Farshid Keivanian <87278222+FarshidKeivanian@users.noreply.github.com>
2024-08-20 06:25:54 +1000

    Add files via upload

---
+++ SimpleMathProcessor.py
@@ -0,0 +1,12 @@
+# math_operations.py
+def add(a, b):
+    return a + b
+    
+def subtract(a, b):
+    return a - b
+    
+# main.py
+import math_operations
+
+result = math_operations.add(5, 3)
+print(result)  # Output: 8
