5586d5598a7af5c05c2d68c9bb55faa1e5b55ab9
Dr. Farshid Keivanian <87278222+FarshidKeivanian@users.noreply.github.com>
2024-09-21 20:44:07 +1000

    Add files via upload

---
+++ Telco Churn Data Cleaner.py
@@ -0,0 +1,13 @@
+import pandas as pd
+df = pd.read_csv(r'C:\IDS201\Telco_Customer_Churn_Dataset_for_ML_Prediction_Analysis.csv')
+
+print(df.head())
+print(df.info())
+print(df.describe())
+
+# Identify and handle missing values
+missing_values = df.isnull().sum()
+print("\nMissing values in each column:")
+print(missing_values)
+
+df.dropna(inplace=True)  # Example of dropping rows with missing values
